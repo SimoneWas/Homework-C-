b82746778b7660e354979f383e519d1f103742e5
Simona Vasileva <149930250+SimoneWas@users.noreply.github.com>
2024-03-14 12:24:03 +0100

    Add files via upload

---
+++ Homework03/StudentGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿//Task 2
+//Make a new console application called StudentGroup
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names (strings).
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+
+string[] studentsG1 = { "Lidija", "Sara", "Gorgi", "David", "Emilija" };
+string[] studentsG2 = { "Simona", "Martina", "Martin", "Petko", "Trajko" };
+
+Console.Write("Enter a number between 1 and 2: ");
+int groupNumber = Convert.ToInt32(Console.ReadLine());
+
+string[] selectedStudents;
+
+if (groupNumber == 1)
+{
+    selectedStudents = studentsG1;
+}
+else
+{
+    selectedStudents = studentsG2;
+}
+
+Console.WriteLine("\nStudents in the selected group:");
+
+foreach (string student in selectedStudents)
+{
+    Console.WriteLine(student);
+}
---
+++ Homework03/SumOfEven/Program.cs
@@ -0,0 +1,17 @@
+﻿//Task 1
+//Make a console application called SumOfEven. Inside it create an array of 6 integers. Find and print the sum of the even numbers from the array:
+
+int[] numbers = { 6,9,7,16,22,8 };
+
+int sumOfEven = 0;
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sumOfEven += number;
+    }
+}
+Console.WriteLine("Sum of Even Numbers: " + sumOfEven);//52
+       
+
+
